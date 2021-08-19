d8f4760cbec747daa69374dec6ba91db8167689c
vcokltfre <vcokltfre@gmail.com>
2021-08-19 19:55:12 +0100

    chore: add http classes to init

--- ablaze/internal/http/__init__.py
+++ ablaze/internal/http/__init__.py
@@ -21,3 +21,14 @@ LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
 OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
 SOFTWARE.
 """
+
+from .client import RESTClient
+from .file import File
+from .route import Route
+
+
+__all__ = (
+    "RESTClient",
+    "File",
+    "Route",
+)
