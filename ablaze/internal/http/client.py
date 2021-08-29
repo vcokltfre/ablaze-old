1928b0a1cf20ebfc464498539b721cf4079159b4
vcokltfre <vcokltfre@gmail.com>
2021-08-29 13:57:32 +0100

    feat: add emoji endpoints

--- ablaze/internal/http/client.py
+++ ablaze/internal/http/client.py
@@ -790,3 +790,59 @@ class RESTClient:
         )
 
         return await (await self.get(route, qparams=params)).json()
+
+    async def list_guild_emojis(self, guild_id: int) -> list:
+        route = Route("/guilds/{guild_id}/emojis", guild_id=guild_id)
+
+        return await (await self.get(route)).json()
+
+    async def get_guild_emoji(self, guild_id: int, emoji_id: int) -> dict:
+        route = Route("/guilds/{guild_id}/emojis/{emoji_id}", guild_id, emoji_id)
+
+        return await (await self.get(route)).json()
+
+    async def create_guild_emoji(
+        self,
+        guild_id: int,
+        name: str,
+        image: str,
+        roles: list = [],
+        *,
+        reason: str = None,
+    ) -> dict:
+        route = Route("/guilds/{guild_id}/emojis", guild_id=guild_id)
+        params = self._get_params(
+            name=name,
+            image=image,
+            roles=roles,
+        )
+
+        return await (await self.post(route, json=params, reason=reason)).json()
+
+    async def modify_guild_emoji(
+        self,
+        guild_id: int,
+        emoji_id: int,
+        name: str = UNSET,
+        roles: list = UNSET,
+        *,
+        reason: str = None,
+    ) -> dict:
+        route = Route(
+            "/guilds/{guild_id}/emojis/{emoji_id}", guild_id=guild_id, emoji_id=emoji_id
+        )
+        params = self._get_params(
+            name=name,
+            roles=roles,
+        )
+
+        return await (await self.post(route, json=params, reason=reason)).json()
+
+    async def delete_guild_emoji(
+        self, guild_id: int, emoji_id: int, *, reason: str = None
+    ) -> None:
+        route = Route(
+            "/guilds/{guild_id}/emojis/{emoji_id}", guild_id=guild_id, emoji_id=emoji_id
+        )
+
+        await self.delete(route, reason=reason)
