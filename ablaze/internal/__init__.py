e13c93c6a943c2afeaa8f14fc73f3a609c4ec775
vcokltfre <vcokltfre@gmail.com>
2021-08-29 13:31:51 +0100

    feat: add channel http routes

--- ablaze/internal/__init__.py
+++ ablaze/internal/__init__.py
@@ -25,7 +25,6 @@ SOFTWARE.
 from .gateway import GatewayClient, Shard
 from .http import File, RESTClient, Route
 
-
 __all__ = (
     "File",
     "RESTClient",
--- ablaze/internal/utils.py
+++ ablaze/internal/utils.py
@@ -22,6 +22,7 @@ OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
 SOFTWARE.
 """
 
+
 class _UNSET:
     """Represents an unset value in a function signature."""
 
