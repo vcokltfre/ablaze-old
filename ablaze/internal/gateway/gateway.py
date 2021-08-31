42491caac800e77e8c0f1ea492312c77497e60d9
vcokltfre <vcokltfre@gmail.com>
2021-08-31 15:10:47 +0100

    fix: use run_forever() in correct class

--- ablaze/internal/gateway/gateway.py
+++ ablaze/internal/gateway/gateway.py
@@ -60,8 +60,6 @@ class GatewayClient:
             await limiter.wait()
             self._loop.create_task(shard.connect())
 
-        self._loop.run_forever()
-
     async def dispatch(self, shard: Shard, direction: str, event: dict) -> None:
         name = event.get("t") or f"OP_{event['op']}"
 
